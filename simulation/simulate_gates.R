9213546862623b1a6b2fbfe6aa379faa97ed634a
Talar1997 <32823145+Talar1997@users.noreply.github.com>
2021-05-06 21:25:37 +0200

    Update
    Wykresy, dodany kod do sprawozdania, poprawki w kodzie

--- simulation/simulate_gates.R
+++ simulation/simulate_gates.R
@@ -3,14 +3,6 @@
 # Created by: Adam Talarczyk, Mateusz Wrzol
 # Created on: 01.05.2021
 
-# ilość aut na minute (z tego co widzę to najbardziej pasuje
-# dokladać tu co pół auta na minute czyli najpierw 0.5 auta na minute, 1 auto na minute, 15 auta itd.)
-# Wtedy widać jak wydłuża się średni czas oczekiwania
-# trzeba pamiętać, że kierowców płacących kartą i gotówką puszczamy nie zależnie czyli
-# traffic_param = 0.5 oznacza de facto 1 kierowce na minute,traffic_param = 1.5 oznacza 3 kierowców na minute łącznie itd.
-# Myślę, że spoko będzie pokazać, że od pewnej wartości sredni
-# czas oczekiwania nie wzrasta już (np jak wszyscy kierowcy przyjadą
-# w ciągu minuty lub dwóch)
 simulate_gates <- function(cars_per_minute = 2,
                            cash_drivers_trajectory,
                            card_drivers_trajectory,
@@ -23,11 +15,11 @@ simulate_gates <- function(cars_per_minute = 2,
       add_resource("gate3", 1) %>%
       add_resource("gate4", 1) %>%
       add_generator("CASH event ",
-                    cash_drivers_trajectory,
-                    function() { c(0, rexp(cash_drivers_num - 1, cars_per_minute), -1) }) %>%
+        cash_drivers_trajectory,
+        function() { c(0, rexp(cash_drivers_num - 1, cars_per_minute), -1) }) %>%
       add_generator("CARD event ",
-                    card_drivers_trajectory,
-                    function() { c(0, rexp(card_drivers_num - 1, cars_per_minute), -1) })
+        card_drivers_trajectory,
+        function() { c(0, rexp(card_drivers_num - 1, cars_per_minute), -1) })
 
   gates %>% run(until = 2000)
 
